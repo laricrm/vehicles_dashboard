f5a352fd9228de9cc2c30961f5425aa2b5a9c44f
Larissa <laricrm@hotmail.com>
2026-05-08 08:26:42 -0300

    improve app interface and functionality

--- app.py
+++ app.py
@@ -2,20 +2,20 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-st.header('Análise de Veículos')
+st.header('Vehicles analysis')
 
 # carregar dados
 car_data = pd.read_csv('vehicles.csv')
 
 # botão histograma
-if st.button('Mostrar histograma'):
-    st.write('Distribuição do odômetro')
+if st.button('Show histogram'):
+    st.write('Odometer distribution')
     fig = px.histogram(car_data, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
 
 # botão scatter
-if st.button('Mostrar gráfico de dispersão'):
-    st.write('Preço vs Odômetro')
+if st.button('Show dispersion chart'):
+    st.write('Price vs Odometer')
     fig = px.scatter(car_data, 
                      x="odometer", 
                      y="price"
